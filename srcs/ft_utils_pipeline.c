d22ddf499c423513bb9386d44b0f0482e7567641
Zixianlow <113102979+Zixianlow@users.noreply.github.com>
2023-04-27 19:59:13 +0800

    Fixed echo line and norm

--- srcs/ft_utils_pipeline.c
+++ srcs/ft_utils_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: lzi-xian <lzi-xian@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 18:46:45 by cping-xu          #+#    #+#             */
-/*   Updated: 2023/04/25 14:12:22 by lzi-xian         ###   ########.fr       */
+/*   Updated: 2023/04/25 18:25:49 by lzi-xian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	ft_get_pipe_line2(t_mini *mini)
 	l = 0;
 	k = 0;
 	mini->pipe_line[l] = malloc(sizeof(char *) * ft_calc_list_size(mini, i));
-	mini->echo_line[l] = malloc(sizeof(char *) * ft_calc_list_size(mini, i));
 	while (mini->line_list[i] && ft_strncmp(mini->line_list[i], "|", 2))
 	{
 		mini->pipe_line[l][k] = ft_strdup(mini->line_list[i++]);
@@ -38,7 +37,28 @@ void	ft_get_pipe_line2(t_mini *mini)
 		}
 	}
 	mini->pipe_line[l][k] = NULL;
-	mini->echo_line[l][k] = NULL;
+}
+
+void	ft_get_echo_line(t_mini *mini)
+{
+	int	l;
+	int	k;
+
+	l = 0;
+	while (mini->pipe_line[l])
+		l++;
+	mini->echo_line = malloc(sizeof(char **) * l + 1);
+	mini->echo_line[l] = NULL;
+	l = 0;
+	while (mini->pipe_line[l])
+	{
+		k = 0;
+		while (mini->pipe_line[l][k])
+			k++;
+		mini->echo_line[l] = malloc(sizeof(char *) * k + 1);
+		mini->echo_line[l][k] = NULL;
+		l++;
+	}
 }
 
 void	ft_get_pipe_line(t_mini *mini)
@@ -58,8 +78,7 @@ void	ft_get_pipe_line(t_mini *mini)
 	if (i > 0)
 		l++;
 	mini->pipe_line = malloc(sizeof(char **) * l);
-	mini->echo_line = malloc(sizeof(char **) * l);
 	mini->pipe_line[l - 1] = NULL;
-	mini->echo_line[l - 1] = NULL;
 	ft_get_pipe_line2(mini);
+	ft_get_echo_line(mini);
 }
